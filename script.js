d884d555a20189ccafed09f6441f5ced0abc3964
Jordan Maurice Beaumont <jordanbmnt@gmail.com>
2024-01-15 13:55:44 +0200

    added style

--- script.js
+++ script.js
@@ -12,5 +12,10 @@ for (const card of cards) {
     card.children[0].style.transform = cardTransformation
       ? ""
       : "rotateY(180deg)";
+      const classList = card.children[0].classList;
+      if(classList.contains("flip")) card.children[0].classList.remove("flip");
+      else card.children[0].classList.add("flip");
+      console.log(classList)
   });
 }
+
