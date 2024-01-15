566cf9af074f5aebe7f6e5e1b1c73185a171f4a7
Jordan Maurice Beaumont <jordanbmnt@gmail.com>
2024-01-15 12:34:35 +0200

    added content to the personality card

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const cards = document.getElementsByClassName("card");
 
+function redirect() {
+  window.open("https://www.16personalities.com/infj-personality", "_blank");
+}
+
+document.getElementById("personality-link").addEventListener("click", redirect);
+
 for (const card of cards) {
   card.addEventListener("click", () => {
     const cardTransformation = card.children[0].style.transform;
