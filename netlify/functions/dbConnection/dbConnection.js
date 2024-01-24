e17134ee2dc8d729b445b6d426ca3c65c5c1742f
Jordan Maurice Beaumont <jordanbmnt@gmail.com>
2024-01-24 15:54:47 +0200

    added an insert for my db

--- netlify/functions/dbConnection/dbConnection.js
+++ netlify/functions/dbConnection/dbConnection.js
@@ -7,11 +7,26 @@ const clientPromise = mongoClient.connect();
 const handler = async (event) => {
   try {
     const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
+    console.log(database.collections);
     const collection = database.collection(process.env.MONGODB_COLLECTION);
-    // Function logic here ...
+
+    //Add a contact
+
+    const newItem = {
+      fname: "Plastic Bricks",
+      lname: "j",
+      subject: "nknasjnc",
+    };
+    collection.insertOne(newItem);
+
+    // find a contact
+
+    const results = await collection.find({}).limit(10).toArray();
+    console.log(results);
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ hel: "collection" }),
+      body: results.toString(),
     };
   } catch (error) {
     return { statusCode: 500, body: error.toString() };
